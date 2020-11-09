2217aaae460ef33993069e11d10c80e0b3920f7f
ledinhtuyenbkdn <ledinhtuyenbkdn@gmail.com>
2020-11-09 15:49:59 +0700

    feat: update code

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,5 @@
 
-J
-src/main/java/app/App.java,1\0\10edb266200144cf4bfe18044835691fa5d6a4b4
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/main/java/app/Deidentify.java,3\e\3e4331c1b39206ae939850f4581f007014d9d01b
--- src/main/java/app/Deidentify.java
+++ src/main/java/app/Deidentify.java
@@ -1,9 +1,5 @@
 package app;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.data.ElementDictionary;
 import org.dcm4che3.data.Tag;
@@ -17,75 +13,37 @@ import org.dcm4che3.tool.common.CLIUtils;
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Deidentify {
 
-    private static ResourceBundle rb = ResourceBundle.getBundle("messages");
-
     private final DeIdentifier deidentifier;
-    private DicomEncodingOptions encOpts = DicomEncodingOptions.DEFAULT;
 
     public Deidentify(DeIdentifier.Option... options) {
         deidentifier = new DeIdentifier(options);
     }
 
-    public void setEncodingOptions(DicomEncodingOptions encOpts) {
-        this.encOpts = encOpts;
-    }
-
-    @SuppressWarnings("static-access")
-    private static CommandLine parseComandLine(String[] args) throws ParseException {
-        Options opts = new Options();
-        CLIUtils.addCommonOptions(opts);
-        CLIUtils.addEncodingOptions(opts);
-        opts.addOption(Option.builder()
-                .desc(rb.getString("retain-date"))
-                .longOpt("retain-date")
-                .build());
-        opts.addOption(Option.builder()
-                .desc(rb.getString("retain-org"))
-                .longOpt("retain-org")
-                .build());
-        opts.addOption(Option.builder()
-                .desc(rb.getString("retain-dev"))
-                .longOpt("retain-dev")
-                .build());
-        opts.addOption(Option.builder()
-                .desc(rb.getString("retain-uid"))
-                .longOpt("retain-uid")
-                .build());
-        opts.addOption(Option.builder("s")
-                .hasArgs().argName("attr=value").valueSeparator('=')
-                .desc(rb.getString("set"))
-                .build());
-        CommandLine cl = CLIUtils.parseComandLine(args, opts, rb, Deidentify.class);
-        return cl;
-    }
-
     public static void main(String[] args) {
         try {
-            CommandLine cl = parseComandLine(args);
-            Deidentify main = new Deidentify(options(cl));
-            main.setEncodingOptions(CLIUtils.encodingOptionsOf(cl));
-            main.setDummyValues(cl.getOptionValues("s"));
-            final List<String> argList = cl.getArgList();
-            int argc = argList.size();
-            if (argc < 2)
-                throw new ParseException(rb.getString("missing"));
-            File dest = new File(argList.get(argc-1));
-            if ((argc > 2 || new File(argList.get(0)).isDirectory())
-                    && !dest.isDirectory())
-                throw new ParseException(
-                        MessageFormat.format(rb.getString("nodestdir"), dest));
-            for (String src : argList.subList(0, argc-1))
-                main.mtranscode(new File(src), dest);
-        } catch (ParseException e) {
-            System.err.println("deidentify: " + e.getMessage());
-            System.err.println(rb.getString("try"));
-            System.exit(2);
+            DeIdentifier.Option[] options = new DeIdentifier.Option[]{
+                    DeIdentifier.Option.RetainUIDsOption,
+//                    DeIdentifier.Option.RetainInstitutionIdentityOption,
+//                    DeIdentifier.Option.RetainDeviceIdentityOption,
+//                    DeIdentifier.Option.RetainLongitudinalTemporalInformationFullDatesOption,
+//                    DeIdentifier.Option.BasicApplicationConfidentialityProfile
+            };
+
+            Map<String, String> dummyValues = new HashMap<>();
+            dummyValues.put("PatientName", "ANONYMIZED");
+
+            String srcPath = "./data/in";
+            String destPath = "./data/out";
+
+            Deidentify main = new Deidentify(options);
+            main.setDummyValues(dummyValues);
+
+            main.mtranscode(new File(srcPath), new File(destPath));
         } catch (Exception e) {
             System.err.println("deidentify: " + e.getMessage());
             e.printStackTrace();
@@ -93,26 +51,12 @@ public class Deidentify {
         }
     }
 
-    private void setDummyValues(String[] optVals) {
-        if (optVals != null)
-            for (int i = 1; i < optVals.length; i++, i++) {
-                int tag = CLIUtils.toTag(optVals[i - 1]);
-                VR vr = ElementDictionary.getStandardElementDictionary().vrOf(tag);
-                deidentifier.setDummyValue(tag, vr, optVals[i]);
-            }
-    }
-
-    private static DeIdentifier.Option[] options(CommandLine cl) {
-        EnumSet<DeIdentifier.Option> options = EnumSet.noneOf(DeIdentifier.Option.class);
-        if (cl.hasOption("retain-date"))
-            options.add(DeIdentifier.Option.RetainLongitudinalTemporalInformationFullDatesOption);
-        if (cl.hasOption("retain-dev"))
-            options.add(DeIdentifier.Option.RetainDeviceIdentityOption);
-        if (cl.hasOption("retain-org"))
-            options.add(DeIdentifier.Option.RetainInstitutionIdentityOption);
-        if (cl.hasOption("retain-uid"))
-            options.add(DeIdentifier.Option.RetainUIDsOption);
-        return options.toArray(new DeIdentifier.Option[0]);
+    private void setDummyValues(Map<String, String> dummyValues) {
+        for (Map.Entry<String, String> dummyValue : dummyValues.entrySet()) {
+            int tag = CLIUtils.toTag(dummyValue.getKey());
+            VR vr = ElementDictionary.getStandardElementDictionary().vrOf(tag);
+            deidentifier.setDummyValue(tag, vr, dummyValue.getValue());
+        }
     }
 
     private void mtranscode(File src, File dest) {
@@ -127,11 +71,11 @@ public class Deidentify {
         try {
             transcode(src, dest);
             System.out.println(
-                    MessageFormat.format(rb.getString("deidentified"),
+                    MessageFormat.format("{0} -> {1}",
                             src, dest));
         } catch (Exception e) {
             System.out.println(
-                    MessageFormat.format(rb.getString("failed"),
+                    MessageFormat.format("Failed to de-identify {0}: {1}",
                             src, e.getMessage()));
             e.printStackTrace(System.out);
         }
@@ -149,7 +93,7 @@ public class Deidentify {
         if (fmi != null)
             fmi = dataset.createFileMetaInformation(fmi.getString(Tag.TransferSyntaxUID));
         try (DicomOutputStream dos = new DicomOutputStream(dest)) {
-            dos.setEncodingOptions(encOpts);
+            dos.setEncodingOptions(DicomEncodingOptions.DEFAULT);
             dos.writeDataset(fmi, dataset);
         }
     }
